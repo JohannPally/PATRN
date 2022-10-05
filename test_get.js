12d3abbc937bd16c84b35aee8c74389d0024851a
Johann Pally <pallyjohann@gmail.com>
2022-10-05 12:03:31 -0500

    Imported snoopraw and tested get data

---
+++ test_get.js
@@ -0,0 +1,11 @@
+import snoowrap from 'snoowrap'
+
+const r = new snoowrap({
+    user_agent: "Reddit: PATRN:1.0 (by u/<uwukungfuwu>)",
+    client_id: "d-7c-MSgBBHA0Mirq5Z3cw",
+    client_secret: "EZzaLo5N0p-jmXJYdSF74j7Z7er1VQ",
+    username: 'mace_user_account',
+    password: 'macebot123'
+  });
+  
+  r.getHot().map(post => post.title).then(console.log);
